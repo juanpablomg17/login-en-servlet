ee8be44edbab96d7e9839e28924a88790c893187
Juan Pabo <juanpablomg17@gmail.com>
2020-02-17 10:27:13 -0500

    archivos-agregados

---
+++ src/java/Servelt/Login.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servelt;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Wilsom
+ */
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Login</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Login at "+ request.getParameter("user")+ "</h1>");
+            out.println("<h1>Servlet Login at "+ request.getParameter("contrasena")+ "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response)
+            throws ServletException, IOException { 
+        
+        String user = req.getParameter("user");
+        String pass = req.getParameter("contrasena");
+        if ("eduardo".equals("user") && "elpro2000".equals(pass) ){
+            
+            response(response, "login ok");
+            
+            
+        
+        }else {
+            
+            response(response," Invalid Login ");
+            
+            
+            
+            
+            
+            
+        }
+        
+        
+        
+        processRequest(req, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    private void response(HttpServletResponse response, String estado) throws IOException {
+     
+        
+        PrintWriter out = response.getWriter();
+        
+        
+        
+    
+        
+        
+    }
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,33 @@
+<%-- 
+    Document   : index
+    Created on : 17/02/2020, 09:51:27 AM
+    Author     : Wilsom
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        
+        
+        <div>
+            <form action="Login" method="get">
+                
+                <input type="text" name="user" placeholder="Usuario"/> <br/> 
+                <input type="password" name="contrasena" placeholder="Contraseña"/> <br/>
+                <input type="submit" />
+                
+                
+                
+                
+            </form>
+            
+            
+            
+        </div>
+    </body>
+</html>
